0fb6fcdb96a9ae318e73c802a4c90c81b9294cde
gachagajnr <williampius17@users.noreply.github.com>
2024-02-19 20:00:56 +0300

    set up character component

--- src/components/Character/character.jsx
+++ src/components/Character/character.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import Image from "next/Image";
 
-const character = () => {
-  return <div>character</div>;
+const character = ({
+  name,
+  status,
+  species,
+  type,
+  gender,
+  location,
+  origin,
+  image,
+}) => {
+  return (
+    <div className="flex flex-row p-4 justify-center gap-2 border   border-gray-700 rounded-lg">
+      <div>
+        <Image src={""} width={150} height={150} alt="" />
+      </div>
+      <div>
+        <h1>
+          <span className="text-sm font-bold">Name:</span>
+          {character.name}
+        </h1>
+        <h2>
+          <span className="text-sm font-bold">Status: </span>
+          {character.status}
+        </h2>
+        <h2>
+          <span className="text-sm font-bold">Status: </span>
+          {character.species}
+        </h2>
+        <h2>
+          <span className="text-sm font-bold">Gender: </span>
+          {character.gender}
+        </h2>
+        <h2>
+          <span className="text-sm font-bold">Type: </span>
+          {character.type}
+        </h2>
+        <h2>
+          <span className="text-sm font-bold">Location: </span>
+          {character.location.name}
+        </h2>
+        <h2>
+          <span className="text-sm font-bold">Resident Url: </span>
+          {character.location.url}
+        </h2>
+        <h2>
+          <span className="text-sm font-bold">Image: </span>
+          {character.image}
+        </h2>
+        <h2>
+          <span className="text-sm font-bold">Url: </span>
+          {character.url}
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default character;
