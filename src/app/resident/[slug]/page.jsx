bf3b77d14892041dcc096f98357bdda7d747ed83
gachagajnr <williampius17@users.noreply.github.com>
2024-02-19 21:42:05 +0300

    getting data for single character

--- src/app/resident/[slug]/page.jsx
+++ src/app/resident/[slug]/page.jsx
@@ -1,6 +1,23 @@
+"use client";
 import React from "react";
+import { getCharacter } from "../lib/api";
 
-const Detail = () => {
+const Detail = ({ params }) => {
+  console.log(params);
+
+  useEffect(() => {
+    const fetchCharacter= async () => {
+      try {
+        const response = await getCharacter();
+        console.log(response);
+        setCharacters(response);
+      } catch (error) {
+        console.error("Error fetching characters:", error);
+      }
+    };
+
+    fetchCharacter();
+  }, []);
   return <div>Detail</div>;
 };
 
